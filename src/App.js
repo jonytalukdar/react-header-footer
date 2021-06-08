707186d7d7bb505481158ec66b284cd7ce9d03c5
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-08 16:18:59 +0600

    added card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import Products from './components/Products/Products';
+import SignUp from './components/SignUp.js/SignUp';
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
+        </Switch>
       </Router>
     </>
   );
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../Button/Button';
+import '../../App.css';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for ?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonSize="btn--large"
+          buttonStyle="btn--outline"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonSize="btn--large"
+          buttonStyle="btn--primary"
+        >
+          WATCH TRAILER <i className="far fa-play-circle"></i>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
